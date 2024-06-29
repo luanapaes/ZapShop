45a58f1e275a4dcdd06b48119d5f7b9c7a7691bd
Luana Paes <luanafranca598@gmail.com>
2024-06-28 22:14:23 -0300

    adiciona imagem, componente FaixaAnimada e componente ButtonBrand

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './shared/components/header/header.component';
+import { FaixaAnimadaComponent } from './shared/components/faixa-animada/faixa-animada.component';
+import { ButtonBrandComponent } from './shared/components/button-brand/button-brand.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent],
+  imports: [RouterOutlet, HeaderComponent, FaixaAnimadaComponent, ButtonBrandComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
