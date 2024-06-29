1bde3035a4619e3c213bdd18c9a1b4d08785969c
Luana Paes <luanafranca598@gmail.com>
2024-06-28 22:11:13 -0300

    cria componente FaixaAnimada

---
+++ src/app/shared/components/faixa-animada/faixa-animada.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-faixa-animada',
+  standalone: true,
+  imports: [],
+  templateUrl: './faixa-animada.component.html',
+  styleUrl: './faixa-animada.component.scss'
+})
+export class FaixaAnimadaComponent {
+
+}
