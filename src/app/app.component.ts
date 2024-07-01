7f006b9442f47a82da13593ff0c79e3d4148dd07
Luana Paes <luanafranca598@gmail.com>
2024-06-30 21:25:32 -0300

    adiciona CardMarca ao componente App

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,13 @@ import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './shared/components/header/header.component';
 import { FaixaAnimadaComponent } from './shared/components/faixa-animada/faixa-animada.component';
 import { ButtonBrandComponent } from './shared/components/button-brand/button-brand.component';
+import { CardMarcaComponent } from './shared/components/card-marca/card-marca.component';
+import { FooterComponent } from './shared/components/footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, FaixaAnimadaComponent, ButtonBrandComponent],
+  imports: [RouterOutlet, HeaderComponent, FaixaAnimadaComponent, ButtonBrandComponent, CardMarcaComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
