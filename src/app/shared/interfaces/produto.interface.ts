06090f40052c506e1da8d011d9abc895ac17eee9
Luana Paes <luanafranca598@gmail.com>
2024-06-30 22:52:55 -0300

    cria interface Produto

---
+++ src/app/shared/interfaces/produto.interface.ts
@@ -0,0 +1,8 @@
+export interface Produto {
+    product_id: string;
+    product_name: string;
+    product_price: string;
+    product_description: string, 
+    product_marca: string;
+    product_categoria: string
+}
