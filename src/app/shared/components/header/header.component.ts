6dfec58e609db4c70b7d96b2feb359ace668a234
Luana Paes <luanafranca598@gmail.com>
2024-07-01 22:46:10 -0300

    adiciona condições para exibir ou não determinados elementos do header

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -1,12 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
-
+  @Input() isHome: boolean = true;
 }
