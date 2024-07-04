6cf531a2d3c3202b95af8a2c9e6018a04952e281
Luana Paes <luanafranca598@gmail.com>
2024-07-04 18:58:43 -0300

    cria objeto Produto e envia suas propriedades para o ButtonAddToBag

--- src/app/shared/components/card-produto/card-produto.component.ts
+++ src/app/shared/components/card-produto/card-produto.component.ts
@@ -1,5 +1,6 @@
-import { Component, Input} from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { ButtonAddToBagComponent } from '../button-add-to-bag/button-add-to-bag.component';
+import { Produto } from '../../interfaces/produto.interface';
 
 @Component({
   selector: 'app-card-produto',
@@ -15,4 +16,19 @@ export class CardProdutoComponent {
   @Input() productDescription: string = '';
   @Input() productCategory: string = '';
   @Input() productMarca: string = '';
+
+  product!: Produto;
+
+  constructor(){}
+
+  ngOnInit(): void {
+    this.product = {
+      product_id: this.productId,
+      product_name: this.productName,
+      product_price: this.productPrice,
+      product_description: this.productDescription,
+      product_marca: this.productMarca,
+      product_categoria: this.productCategory
+    }
+  }
 }
