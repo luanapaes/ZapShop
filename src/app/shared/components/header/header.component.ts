2f63fb07c9208f63b5a559153ecb41ac50d3dafc
Luana Paes <luanafranca598@gmail.com>
2024-07-05 22:38:00 -0300

    adiciona botão para cadastrar novos produtos

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -1,23 +1,30 @@
-import { Component, EventEmitter, Input, Output, inject } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { CartComponent } from '../cart/cart.component';
 import { MatDialog } from '@angular/material/dialog';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatButtonModule } from '@angular/material/button';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink, MatButtonModule, MatMenuModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
   @Input() isHome: boolean = true;
-  constructor(public dialog: MatDialog) { }
+  constructor(public dialog: MatDialog, private router: Router) { }
 
   openCart() {
     this.dialog.open(CartComponent).
     afterClosed().subscribe(result => {
       console.log(`Dialog result: ${result}`);
     });
+  }
+
+  goToAddProdutos(){
+    this.router.navigate(['cadastrar-produto'])
   }
 }
