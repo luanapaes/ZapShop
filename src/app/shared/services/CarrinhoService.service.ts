07fee8b9be440b529bb5a97d6fd4a07eb8176a77
Luana Paes <luanafranca598@gmail.com>
2024-07-15 18:01:02 -0300

    muda a interface Produto para CartProduct

--- src/app/shared/services/CarrinhoService.service.ts
+++ src/app/shared/services/CarrinhoService.service.ts
@@ -1,16 +1,18 @@
 import { Injectable } from '@angular/core';
 import { Produto } from '../interfaces/produto.interface';
+import { CartProduct } from '../interfaces/cart-product.interface';
 
 @Injectable({
     providedIn: 'root'
 })
 export class CarrinhoService {
-    private produtosCarrinho: Produto[] = [];
+    private produtosCarrinho: CartProduct[] = [];
 
-    addProductToBag(productId: string, productName: string, productImage: string ,productPrice: number, productDescription: string, productMarca: string, productCartegory: string[]) {
-        const product: Produto = {
+    addProductToBag(productId: string, productName: string, productImage: string, productPrice: number, productDescription: string, productMarca: string, productCartegory: string[]) {
+        const product: CartProduct = {
             product_id: productId,
             product_name: productName,
+            qtd_product: 1,
             product_image: productImage,
             product_price: productPrice,
             product_description: productDescription,
@@ -21,7 +23,15 @@ export class CarrinhoService {
         this.produtosCarrinho.push(product);
     }
 
-    getProdutosCarrinho(): Produto[] {
+    getProdutosCarrinho(): CartProduct[] {
         return this.produtosCarrinho;
     }
+
+    delete(id: string): Produto | undefined {
+        return this.produtosCarrinho.find(produto => produto.product_name === id);
+    }
+
+    // delete(name: string): void {
+    //     this.produtosCarrinho = this.produtosCarrinho.filter(produto => produto.product_name !== name);
+    // }
 }
