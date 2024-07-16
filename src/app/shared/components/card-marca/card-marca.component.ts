897ca38f694090a4b08a2ed54540911ffb8a6f9a
Luana Paes <luanafranca598@gmail.com>
2024-07-15 21:06:17 -0300

    remove importações desnecessárias

--- src/app/shared/components/card-marca/card-marca.component.ts
+++ src/app/shared/components/card-marca/card-marca.component.ts
@@ -2,8 +2,6 @@ import { Component, inject, signal } from '@angular/core';
 import { Marca } from '../../interfaces/marca.interface';
 import { NgClass, NgStyle } from '@angular/common';
 import { MarcasService } from '../../services/MarcasService.service';
-import { ActivatedRoute } from '@angular/router';
-import { Observable } from 'rxjs';
 
 
 @Component({
@@ -28,27 +26,4 @@ export class CardMarcaComponent {
       }
     )
   }
-
-  // public listMarcas = signal<Marca[]>([
-  //   {
-  //     nome_marca: "Natura",
-  //     categorias: ['perfume', 'hidratante', 'sabonete'],
-  //     url_logo: '../../../../assets/marcas/logo-natura.png'
-  //   },
-  //   {
-  //     nome_marca: "oBoticario",
-  //     categorias: ['perfume', 'hidratante', 'sabonete'],
-  //     url_logo: '../../../../assets/marcas/logo-oboticario.jpg'
-  //   },
-  //   {
-  //     nome_marca: "Eudora",
-  //     categorias: ['perfume', 'hidratante', 'sabonete'],
-  //     url_logo: '../../../../assets/marcas/logo-eudora2.png'
-  //   },
-  //   {
-  //     nome_marca: "Hinode",
-  //     categorias: ['perfume', 'hidratante', 'sabonete'],
-  //     url_logo: '../../../../assets/marcas/logo-hinode2.png'
-  //   },
-  // ])
 }
