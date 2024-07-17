239c25f10c3c24d8b684cec0034b0d3111dab2c3
Luana Paes <luanafranca598@gmail.com>
2024-07-16 21:34:23 -0300

    adiciona withFetch ao provider provideHttpClient

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,14 +4,14 @@ import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
 import { provideAnimations } from '@angular/platform-browser/animations';
-import { provideHttpClient } from '@angular/common/http';
+import { provideHttpClient, withFetch } from '@angular/common/http';
 import { registerLocaleData } from '@angular/common';
 import localePt from '@angular/common/locales/pt';
 
 registerLocaleData(localePt, 'pt');
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideClientHydration(), provideAnimations(), provideHttpClient(),
+  providers: [provideRouter(routes), provideClientHydration(), provideAnimations(), provideHttpClient(withFetch()),
     { provide: LOCALE_ID, useValue: 'pt' },
   ]
 };
