4991759b3b3f10f5547aefe85751c59bf5df285e
Luana Paes <luanafranca598@gmail.com>
2024-07-22 22:39:19 -0300

    adiciona FiltrosComponent

--- src/app/pages/produtos/produtos.component.ts
+++ src/app/pages/produtos/produtos.component.ts
@@ -5,11 +5,12 @@ import { Produto } from '../../shared/interfaces/produto.interface';
 import { FooterComponent } from '../../shared/components/footer/footer.component';
 import { ProdutosService } from '../../shared/services/ProdutosService.service';
 import { ActivatedRoute } from '@angular/router';
+import { FiltrosComponent } from '../../shared/components/filtros/filtros.component';
 
 @Component({
   selector: 'app-produtos',
   standalone: true,
-  imports: [CardProdutoComponent, HeaderComponent, FooterComponent],
+  imports: [CardProdutoComponent, HeaderComponent, FooterComponent, FiltrosComponent],
   templateUrl: './produtos.component.html',
   styleUrl: './produtos.component.scss'
 })
