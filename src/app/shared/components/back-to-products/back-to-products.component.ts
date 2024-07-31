c58115e6d14db63d20d6cc215dea628cd0f80fc9
Luana Paes <luanafranca598@gmail.com>
2024-07-30 21:49:14 -0300

    cria BackToProductsComponent

---
+++ src/app/shared/components/back-to-products/back-to-products.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'app-back-to-products',
+  standalone: true,
+  imports: [RouterLink],
+  templateUrl: './back-to-products.component.html',
+  styleUrl: './back-to-products.component.scss'
+})
+export class BackToProductsComponent {
+
+}
