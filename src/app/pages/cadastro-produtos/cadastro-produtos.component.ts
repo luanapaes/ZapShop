b203d041ea64a6b2b2a76db5c03e6cf086501119
Luana Paes <luanafranca598@gmail.com>
2024-07-30 21:49:54 -0300

    adiciona component back-to-component e ajusta sua visualização

--- src/app/pages/cadastro-produtos/cadastro-produtos.component.ts
+++ src/app/pages/cadastro-produtos/cadastro-produtos.component.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
 import { FormAddProdutoComponent } from '../../shared/components/form-add-produto/form-add-produto.component';
 import { HeaderComponent } from '../../shared/components/header/header.component';
+import { BackToProductsComponent } from '../../shared/components/back-to-products/back-to-products.component';
 
 @Component({
   selector: 'app-cadastro-produtos',
   standalone: true,
-  imports: [HeaderComponent,FormAddProdutoComponent],
+  imports: [HeaderComponent,FormAddProdutoComponent,
+    BackToProductsComponent
+  ],
   templateUrl: './cadastro-produtos.component.html',
   styleUrl: './cadastro-produtos.component.scss'
 })
