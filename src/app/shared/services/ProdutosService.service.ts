39e2c6f862c5b87ba07fc69229e518d4ce28e25c
Luana Paes <luanafranca598@gmail.com>
2024-08-01 17:24:08 -0300

    add deleteProductById function

--- src/app/shared/services/ProdutosService.service.ts
+++ src/app/shared/services/ProdutosService.service.ts
@@ -1,6 +1,7 @@
 import { inject, Injectable } from '@angular/core';
 import { Produto } from '../interfaces/produto.interface';
 import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
     providedIn: 'root'
@@ -9,22 +10,6 @@ export class ProdutosService {
     httpClient = inject(HttpClient);
 
     apiUrl:string = 'http://localhost:3000/produtos'
-
-    // addNewProduct(id: string, nomeProduto: string, productImage: string,
-    //     precoProduto: number, descricaoProduto: string, marcaProduto: string, categoriaProduto: string[]){
-
-    //     const produto: Produto = {
-    //         product_id: id,
-    //         product_name: nomeProduto,
-    //         product_image: productImage,
-    //         product_price: precoProduto,
-    //         product_description: descricaoProduto,
-    //         product_marca: marcaProduto,
-    //         product_categoria: categoriaProduto
-    //     }
-
-    //     this.arrayProducts.push(produto);
-    // }
 
     addNewProduct(id: string, nomeProduto: string, productImage: string,
         precoProduto: number, descricaoProduto: string, marcaProduto: string, categoriaProduto: string[]) {
@@ -46,4 +31,7 @@ export class ProdutosService {
         return this.httpClient.get<Produto[]>(this.apiUrl)
     }
 
+    deleteProductById(product_id: string): Observable<void> {
+        return this.httpClient.delete<void>(`${this.apiUrl}?product_id=${product_id}`);
+    }
 }
