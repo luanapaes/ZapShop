aa4c76677dafbf229c1ac42183a8d450e7b9d58f
Luana Paes <luanafranca598@gmail.com>
2024-09-26 22:44:53 -0300

    feat(login): add FormGroup e configura envio do form através da função onSubmit

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,12 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormGroup, FormControl, FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { LoginService } from '../../shared/services/LoginService.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule, FormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
 export class LoginComponent {
+  loginService = inject(LoginService);
+  router = inject(Router);
 
+  myLoginForm = new FormGroup({
+    email: new FormControl(),
+    password: new FormControl(),
+  });
+
+  onSubmit() {
+    console.log(this.myLoginForm.value.email,
+      this.myLoginForm.value.password)
+    if (this.myLoginForm.valid) {
+      this.loginService.logar(
+        this.myLoginForm.value.email,
+        this.myLoginForm.value.password,
+      ).subscribe(() => {
+        this.router.navigate(['produtos'])
+      })
+    } else {
+      console.log("Não enviado.")
+    }
+  }
 }
