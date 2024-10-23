960ecd09dacf09c47dc5ad3c13e37fe30bd262d4
Luana Paes <luanafranca598@gmail.com>
2024-10-23 15:22:00 -0300

    refactor(header): integra NavOptionsComponent ao header com controle de opções para administrador
    - Verifica a presença do token no localStorage para determinar se o usuário é administrador
    - Exibe opções administrativas no header apenas para usuários autenticados como administradores

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, NgModule } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { CartComponent } from '../cart/cart.component';
 import { MatDialog } from '@angular/material/dialog';
@@ -6,32 +6,30 @@ import { MatMenuModule } from '@angular/material/menu';
 import { MatButtonModule } from '@angular/material/button';
 import { Router } from '@angular/router';
 import { UserDialogComponent } from '../user-dialog/user-dialog.component';
+import { NavOptionsComponent } from './components/nav-options/nav-options.component';
+import { NgStyle } from '@angular/common';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [RouterLink, MatButtonModule, MatMenuModule],
+  imports: [RouterLink, MatButtonModule, MatMenuModule, 
+    NavOptionsComponent, NgStyle],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
   @Input() isHome: boolean = true;
+
+  //verifica se tem token no localStorage
+  adm: boolean = localStorage.getItem('token') !== null; 
   
-  constructor(public dialog: MatDialog, private router: Router) { }
+  constructor(public dialog: MatDialog) {}
 
   openCart() {
     this.dialog.open(CartComponent).
     afterClosed().subscribe(result => {
       console.log(`Dialog result: ${result}`);
     });
-  }
-
-  goToAddProdutos(){
-    this.router.navigate(['cadastrar-produto'])
-  }
-
-  goToAdmPage(){
-    this.router.navigate(['area-adm'])
   }
 
   openUserDialog(){
