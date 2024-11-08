1c02b3d2a85db33231965499bf4c2b946cc58dfe
Luana Paes <luanafranca598@gmail.com>
2024-11-07 22:20:11 -0300

    refactor: remove importações desnecessárias para melhorar a limpeza do código.

--- src/app/shared/components/form-add-produto/form-add-produto.component.ts
+++ src/app/shared/components/form-add-produto/form-add-produto.component.ts
@@ -1,4 +1,4 @@
-import { Component, ElementRef, inject, Signal, signal, SimpleChanges, viewChild, ViewChild } from '@angular/core';
+import { Component, ElementRef, inject, Input, input, Signal, signal, SimpleChanges, viewChild, ViewChild } from '@angular/core';
 import { FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 import { MatInputModule } from '@angular/material/input';
 import { MatSelectModule } from '@angular/material/select';
@@ -8,12 +8,13 @@ import { Router } from '@angular/router';
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { MarcasService } from '../../services/MarcasService.service';
 import { Marca } from '../../interfaces/marca.interface';
+import { CurrencyPipe } from '@angular/common';
 
 @Component({
   selector: 'app-form-add-produto',
   standalone: true,
   imports: [ReactiveFormsModule, FormsModule, MatFormFieldModule,
-    MatSelectModule, MatInputModule],
+    MatSelectModule, MatInputModule, CurrencyPipe],
   templateUrl: './form-add-produto.component.html',
   styleUrl: './form-add-produto.component.scss'
 })
