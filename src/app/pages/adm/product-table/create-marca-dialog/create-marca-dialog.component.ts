df118a1b1d1a0ce75bed7a8f144db7cb56ccdd9d
Luana Paes <luanafranca598@gmail.com>
2024-11-13 21:26:48 -0300

    refactor: ajusta cadastro de marca com melhorias e correções
    - Adiciona o tipo File à propriedade imageSrc
    - Injeta dependências MarcasService, MatSnackBar e Router
    - Corrige o nome da propriedade de 'logomarma' para 'logomarca'
    - Adiciona função onSubmit para envio da marca ao service

--- src/app/pages/adm/product-table/create-marca-dialog/create-marca-dialog.component.ts
+++ src/app/pages/adm/product-table/create-marca-dialog/create-marca-dialog.component.ts
@@ -1,9 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { FormGroup, FormControl, FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
 import { MatDialogTitle, MatDialogContent, MatDialogActions, MatDialogClose } from '@angular/material/dialog';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatSelectModule } from '@angular/material/select';
+import { MarcasService } from '../../../../shared/services/MarcasService.service';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-create-marca-dialog',
@@ -19,12 +22,16 @@ import { MatSelectModule } from '@angular/material/select';
 })
 export class CreateMarcaDialogComponent {
   categoriasList: string[] = ['Perfume', 'Hidratante', 'Kit', 'Maquiagem', 'Hidratação', 'Skincare'];
-  imageSrc: string | ArrayBuffer | null = null;
+  imageSrc: string | ArrayBuffer | File | null = null;
+  
+  marcasService = inject(MarcasService);
+  matSnackBar = inject(MatSnackBar);
+  route = inject(Router)
   
   marcaForm = new FormGroup({
     nome_marca: new FormControl(),
     categorias: new FormControl(),
-    logomarma: new FormControl()
+    logomarca: new FormControl()
   });
 
   onFileSelected(event: Event): void {
@@ -39,6 +46,21 @@ export class CreateMarcaDialogComponent {
       };
 
       reader.readAsDataURL(file);
+    }
+  }
+
+  onSubmit(){
+    if(this.marcaForm.valid){
+      this.marcasService.cadastrarMarca(
+        this.marcaForm.value.nome_marca,
+        this.marcaForm.value.categorias,
+        this.imageSrc as File
+      ).subscribe(() => {
+        this.matSnackBar.open("Marca cadastrada com sucesso!");
+        this.route.navigate(["area-adm"]);
+      })
+    } else{
+      this.matSnackBar.open("Marca não cadastrada.")
     }
   }
 }
