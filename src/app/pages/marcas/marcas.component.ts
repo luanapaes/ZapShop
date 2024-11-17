b865a37454355599789f5851c4af0120b4e99a62
Luana Paes <luanafranca598@gmail.com>
2024-11-16 22:09:24 -0300

    feat(MarcasComponent): implementa carregamento de marcas e abertura de diálogo para criação de marcas
    - adiciona a funcionalidade para carregar marcas através do `MarcasService` e exibir na interface.
    - implementa método para abrir o diálogo de criação de marca.
    - estrutura os métodos `onDelete` e `onEdit`, que ainda serão implementados.

--- src/app/pages/marcas/marcas.component.ts
+++ src/app/pages/marcas/marcas.component.ts
@@ -1,12 +1,45 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { HeaderComponent } from '../../shared/components/header/header.component';
+import { AdmComponent } from "../adm/adm.component";
+import { MarcasService } from '../../shared/services/MarcasService.service';
+import { MatDialog } from '@angular/material/dialog';
+import { CreateMarcaDialogComponent } from '../adm/product-table/create-marca-dialog/create-marca-dialog.component';
+import { CustomMarca } from '../../shared/interfaces/custom-marca.interface';
 
 @Component({
   selector: 'app-marcas',
   standalone: true,
-  imports: [],
+  imports: [HeaderComponent, AdmComponent],
   templateUrl: './marcas.component.html',
   styleUrl: './marcas.component.scss'
 })
 export class MarcasComponent {
+  marcasService = inject(MarcasService);
+  arrayMarcas: CustomMarca[] = [];
 
+  constructor(public dialog: MatDialog) { }
+
+  ngOnInit(): void {
+    this.carregarMarcas()
+  }
+
+  carregarMarcas(){
+    this.marcasService.get().subscribe(
+      (marcas) => {
+        this.arrayMarcas = marcas
+      }
+    )
+  }
+
+  openDialog() {
+    this.dialog.open(CreateMarcaDialogComponent);
+  }
+
+  onDelete(id: string){
+
+  }
+
+  onEdit(marca: CustomMarca){
+
+  }
 }
