42a080869ec05c5ab4208a8c2bbcf8ae4e4f556d
Luana Paes <luanafranca598@gmail.com>
2024-11-25 22:17:27 -0300

    feat: implementar função para editar marca com envio de dados e autenticação via token
    - adiciona a função `editarMarca` para enviar informações de marca (nome, categorias e logomarca) ao backend.
    - usa `FormData` para preparar os dados enviados na requisição HTTP.
    - adiciona suporte à autenticação via token, obtendo o token do local storage e inserindo-o no cabeçalho `Authorization`.
    - garante a conformidade com a API para atualização parcial de marcas.

--- src/app/shared/services/MarcasService.service.ts
+++ src/app/shared/services/MarcasService.service.ts
@@ -3,6 +3,7 @@ import { inject, Injectable, Signal, signal } from '@angular/core';
 import { Marca } from '../interfaces/marca.interface';
 import { Observable } from 'rxjs';
 import { CustomMarca } from '../interfaces/custom-marca.interface';
+import { LoginService } from './LoginService.service';
 
 @Injectable({
     providedIn: 'root'
@@ -11,7 +12,6 @@ export class MarcasService {
     httpClient = inject(HttpClient);
     urlMarcasApi: string = '/api/marcas';
     urlProdutosApi: string = '/api/produtos';
-
     marcaSelecionada = signal<string>('');
 
     //carrega todas as marcas
@@ -54,6 +54,37 @@ export class MarcasService {
 
         return this.httpClient.post<Marca>(this.urlMarcasApi, formData)
     }
+
+    editarMarca(id: string, nomeMarca: string, categorias: string, logomarca: File) {
+        const formData = new FormData();
+
+        if (nomeMarca) {
+            formData.append('nome_marca', nomeMarca);
+        }
+
+        if (categorias) {
+            formData.append('categorias', categorias);
+        }
+
+        if (logomarca) {
+            formData.append('logomarca', logomarca);
+        }
+
+        const marca: Marca = {
+            nome_marca: formData.get('nome_marca') as string,
+            categorias: formData.get('categorias') as string,
+            logomarca: formData.get('logomarca') as string
+        }
+
+        const token = localStorage.getItem('authToken'); //pega o token do localstorage
+
+        const headers = {
+            Authorization: `Bearer ${token}`, //envia o token no cabeçalho
+        };
+
+        return this.httpClient.patch(`${this.urlMarcasApi}/${id}`, marca, { headers });
+    }
+
 
     delete(id: number): Observable<void>{
         return this.httpClient.delete<void>(`${this.urlMarcasApi}/${id}`)
