6d71725242258d6d1a5062c408da2d490f036d48
Luana Paes <luanafranca598@gmail.com>
2025-01-14 23:08:39 -0300

    refactor(LoginComponent): aprimora validações e redirecionamento pós-login
    - adiciona validadores aos FormControls para validar e-mail e senha de forma adequada.
    - exibe mensagem de sucesso após login bem-sucedido para melhorar a experiência do usuário.
    - refina o redirecionamento, garantindo uma transição mais fluida após o login.

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,7 +1,8 @@
 import { Component, inject } from '@angular/core';
-import { FormGroup, FormControl, FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { FormGroup, FormControl, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 import { LoginService } from '../../shared/services/LoginService.service';
 import { Router, RouterLink, RouterOutlet } from '@angular/router';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-login',
@@ -17,20 +18,31 @@ export class LoginComponent {
   loginService = inject(LoginService);
   router = inject(Router);
 
+  constructor(private _snackBar: MatSnackBar){}
+
   myLoginForm = new FormGroup({
-    email: new FormControl(),
-    password: new FormControl(),
+    email: new FormControl("", [
+      Validators.required,
+      Validators.email
+    ]),
+    password: new FormControl("", [
+      Validators.required,
+      Validators.minLength(6)
+    ]),
   });
 
   onSubmit() {
-    console.log(this.myLoginForm.value.email,
-      this.myLoginForm.value.password)
     if (this.myLoginForm.valid) {
       this.loginService.logar(
-        this.myLoginForm.value.email,
-        this.myLoginForm.value.password,
+        this.myLoginForm.value.email as string,
+        this.myLoginForm.value.password as string,
       ).subscribe(() => {
-        this.router.navigate(['produtos'])
+        this._snackBar.open('Aguarde..', '', {
+          duration: 1200
+        });
+        setTimeout(() => {
+          this.router.navigate(['produtos'])
+        }, 2000);
       })
     } else {
       console.log("Não enviado.")
