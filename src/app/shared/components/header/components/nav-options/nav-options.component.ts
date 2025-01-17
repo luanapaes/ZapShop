989ba75e2d561fb9b8d2f82175e0266670ac5b82
Luana Paes <luanafranca598@gmail.com>
2025-01-16 23:18:17 -0300

    feat(NavOptionsComponent): adiciona feedback visual ao sair da conta

--- src/app/shared/components/header/components/nav-options/nav-options.component.ts
+++ src/app/shared/components/header/components/nav-options/nav-options.component.ts
@@ -4,6 +4,7 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatMenuModule } from '@angular/material/menu';
 import { Router } from '@angular/router';
 import { LoginService } from '../../../../services/LoginService.service';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-nav-options',
@@ -14,7 +15,10 @@ import { LoginService } from '../../../../services/LoginService.service';
 })
 export class NavOptionsComponent {
   loginService = inject(LoginService)
-  constructor(private router: Router){}
+  constructor(
+    private router: Router,
+    private _snackBar: MatSnackBar
+  ){}
   
   @Input() isADM: boolean = true;
 
@@ -31,6 +35,11 @@ export class NavOptionsComponent {
   }
 
   deslogar(){
-    this.loginService.deslogar()
+    this._snackBar.open('Saindo..', '', {
+      duration: 1200
+    });
+    setTimeout(() => {
+      this.loginService.deslogar()
+    }, 2000);
   }
 }
